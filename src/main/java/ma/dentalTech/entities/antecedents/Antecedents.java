2889da1ebbc012ed74e42eb529c35d644e663750
Othmane <otmanelaafar17@gmail.com>
2025-11-03 20:02:13 +0100

    example commit

---
+++ src/main/java/ma/dentalTech/entities/antecedents/Antecedents.java
@@ -0,0 +1,9 @@
+package ma.dentalTech.entities.antecedents;
+
+
+public class Antecedents {
+    private long id_antecedent;
+    private String nom;
+    private String categorie;
+    private String niveauDeRisque;
+}
---
+++ src/main/java/ma/dentalTech/entities/facture/Facture.java
@@ -0,0 +1,5 @@
+package ma.dentalTech.entities.facture;
+
+public class Facture {
+
+}
