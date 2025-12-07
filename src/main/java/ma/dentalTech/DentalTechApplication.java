d1aff28ab46ccf7451e4b3aa9753938d0a294a36
dounia <dounia.khairi0@gmail.com>
2025-12-07 20:18:31 +0100

    https://github.com/othmanelaafar-lang/cabinet_dentaire.git

---
+++ src/main/java/ma/dentalTech/DentalTechApplication.java
@@ -0,0 +1,11 @@
+package ma.dentalTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalTechApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DentalTechApplication.class, args);
+    }
+}
