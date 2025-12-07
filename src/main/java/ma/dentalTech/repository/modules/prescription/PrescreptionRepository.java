c789450aaaecd6a196f85d15435d6eaa4aea5638
dounia <dounia.khairi0@gmail.com>
2025-12-07 20:28:21 +0100

    https://github.com/othmanelaafar-lang/cabinet_dentaire.git

---
+++ src/main/java/ma/dentalTech/repository/modules/prescription/PrescreptionRepository.java
@@ -0,0 +1,8 @@
+package ma.dentalTech.repository.modules.prescription;
+
+import ma.dentalTech.entities.prescreption.Prescreption;
+import ma.dentalTech.repository.common.CrudRepository;
+
+public interface PrescreptionRepository extends CrudRepository<Prescreption, Long> {
+    // Méthodes spécifiques aux Prescreptions peuvent être ajoutées ici
+}
